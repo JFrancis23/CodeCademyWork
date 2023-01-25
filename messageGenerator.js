064b7f8f678d5c27e7624133bc1d0406b0878337
JFrancis23 <95380823+JFrancis23@users.noreply.github.com>
2023-01-25 22:08:32 +0000

    First Draft

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,10 +1,47 @@
 // Random Message Generator
-// Astrology advice
+// Afternoon tea planner
 
-const feelingChoise = [];
-const sign = [];
-const action = [];
+const preposal = [
+    "Would you like",
+    "How do you fancy",
+    "How about",
+    "I'm thinking more like",
+    "I fancy",
+    "Something like",
+    "Considered",
+    "Would you enjoy",
+    "I like the sound of",
+    "I think you would like",
 
-let fortune = [];
+];
+const tea = [
+    "English breakfast tea",
+    "chamomile tea",
+    "white tea",
+    "oolong tea",
+    "green tea",
+    "tea with biscuits",
+    "tea and scones",
+    "afternoon tea",
+    "a mug of tea",
+    "a spot of tea",
 
-// New stuff
+];
+const location = [
+    "at yours?",
+    "at mine?",
+    "at Starbucks?",
+    "at coffee 1?",
+    "at the cafe?",
+    "at home?",
+    "by the river?",
+    "at the park?",
+    "at Costa?",
+    "in town?",
+];
+
+function random() {
+    return (Math.floor(Math.random() * 10));
+}
+
+console.log(`${preposal[random()]} ${tea[random()]} ${location[random()]}`);
